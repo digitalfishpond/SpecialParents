6e6d339d3afd6665ccd69b6463bf4b45aeb8107a
Denis Poisson <poisson_1@hotmail.com>
2015-09-25 15:25:09 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module("myApp", []);
+
+myApp.controller("mainController", ['$scope' , function($scope){
+
+	$scope.myDataRef = new Firebase('https://wklkeuwy3lh.firebaseio-demo.com/');
+		
+	$scope.checkEnter = function(keyEvent) {
+        if (keyEvent.which === 13) {
+          $scope.myDataRef.push({user: $scope.nameInput , text: $scope.messageInput});
+          $scope.messageInput = "";
+		}
+	};
+	
+	$scope.myDataRef.on('value' , function(snapshot) {
+		console.log(snapshot.val());
+		$scope.messages = snapshot.val();
+
+		
+	});
+
+}]);
